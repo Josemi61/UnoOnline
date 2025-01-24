3868a38da1ffbe5461cb0423818dc22c272b349e
Josemi61 <jtorcan2903@g.educaand.es>
2025-01-24 13:45:27 +0100

    BackEnd Login y Resgistro

---
+++ BackEnd/V-1 Login, Register/UnoOnline/DTO/UserCreateDTO.cs
@@ -0,0 +1,21 @@
+﻿namespace UnoOnline.DTO
+{
+    public class UserDTO
+    {
+        public long Id { get; set; }
+        public string? Avatar { get; set; }
+        public string? Apodo { get; set; }
+        public string? Email { get; set; }
+    }
+
+    public class UserCreateDTO
+    {
+        public long Id { get; set; }//IFormFile?
+        public  IFormFile? Avatar { get; set; }
+        public string? Apodo { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+    }
+
+
+}
---
+++ BackEnd/V-1 Login, Register/UnoOnline/DataMappers/UserMapper.cs
@@ -0,0 +1,25 @@
+﻿using UnoOnline.DTO;
+using UnoOnline.Models;
+
+namespace UnoOnline.DataMappers
+{
+    public class UserMapper
+    {
+        public UserDTO userToDTO(User user)
+        {
+            return new UserDTO
+            {
+                Id = user.Id,
+                Avatar = user.Avatar,
+                Apodo = user.Apodo,
+                Email = user.Email,
+            };
+        }
+
+        public IEnumerable<UserDTO> usersToDTO(IEnumerable<User> users)
+        {
+            return users.Select(userToDTO);
+        }
+
+    }
+}
