26e2aaa9afc511f2e307b3e04d88098f45d304b0
José Miguel Toro <jtorcan2903@g.educaand.es>
2025-02-05 16:47:04 +0100

    V-3 Emparejamiento
    Empezado:
    Salas de juego, invitaciones de juego y unión a salas. Hechas en BackEnd

---
+++ BackEnd/V-3 Emparejamiento/UnoOnline/Repositories/GameRoomRepository.cs
@@ -0,0 +1,54 @@
+﻿using UnoOnline.Models;
+using UnoOnline.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using UnoOnline.Interfaces;
+
+namespace UnoOnline.Repositories
+{
+    public class GameRoomRepository : IGameRoomRepository
+    {
+        private readonly DataBaseContext _context;
+
+        public GameRoomRepository(DataBaseContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<GameRoom> CreateRoomAsync(int hostId)
+        {
+            var room = new GameRoom { HostId = hostId };
+            _context.GameRooms.Add(room);
+            await _context.SaveChangesAsync();
+            return room;
+        }
+
+        public async Task<GameRoom?> GetRoomByIdAsync(string roomId)
+        {
+            return await _context.GameRooms.FirstOrDefaultAsync(r => r.RoomId == roomId);
+        }
+
+        public async Task<bool> AddGuestToRoomAsync(string roomId, int guestId)
+        {
+            var room = await GetRoomByIdAsync(roomId);
+            if (room == null || room.GuestId != null)
+                return false;
+
+            room.GuestId = guestId;
+            await _context.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task<bool> RemoveRoomAsync(string roomId)
+        {
+            var room = await GetRoomByIdAsync(roomId);
+            if (room == null)
+                return false;
+
+            _context.GameRooms.Remove(room);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+    }
+}
+
